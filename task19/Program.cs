d4a54d30c03cb3e8ae00d95dae43afee800fd98c
Louisa <131443655+Severylol@users.noreply.github.com>
2023-06-02 19:28:45 +0300

    first commit

---
+++ task19/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+{
+Console.Write("Введите пятизначное число: ");
+    string number = Console.ReadLine()!;
+
+if (number.Length != 5 || !int.TryParse(number, out _)) //
+{
+Console.WriteLine("Ошибкочка: Введите пятизначное число!");
+}
+else
+{
+    bool isPalindrome = true;
+for (int i = 0; i < number.Length / 2; i++)
+{
+if (number[i] != number[number.Length - 1 - i])
+{
+    isPalindrome = false;
+break;
+}
+}
+if (isPalindrome)
+{
+Console.WriteLine("Да");
+}
+else
+{
+Console.WriteLine("Нет.");
+}
+}
+}
---
+++ task21/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Координату A(1): ");
+double a1 = double.Parse(Console.ReadLine()!);
+Console.Write("Координату B(1): ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.Write("Координату C(1): ");
+double c1 = double.Parse(Console.ReadLine()!);
+Console.Write("Координату A(2): ");
+double a2 = double.Parse(Console.ReadLine()!);
+Console.Write("Координату B(2): ");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.Write("Координату C(2): ");
+double c2 = double.Parse(Console.ReadLine()!);
+double S = Math.Sqrt(Math.Pow(a1 - a2, 2) + Math.Pow(b2 - b1, 2) + Math.Pow (c1 +c2, 2)); //допустим S -расстоние, нам надо взять квадратный корень Math.Sqrt вещественный кв.корень далее Math.Pow возведение в степень
+Console.WriteLine($"Расстояние равно {Math.Round(S, 3)}");
